366eec9ac74f9112d1c3d427869fc1eaeeb83e49
Andreas Pratama <emailandreas19@gmail.com>
2021-04-22 20:22:37 +0700

    Buat modul acara

---
+++ resources/views/pages/backend/acara/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.backend.admin')
+
+@section('title')
+    Acara
+@endsection
+
+@section('content')
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        </div>
+
+        <!-- Content Row -->
+
+        <div class="row">
+            <div class="card w-100">
+                <h5 class="card-header">
+                    Tambah
+                </h5>
+                <div class="card-body">
+                    <form action="{{route('acara.store')}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                          <label for="deskripsi">Email address</label>
+                          <input type="text" class="form-control" name="deskripsi">
+                        </div>
+                        <div class="form-group">
+                            <label for="photo">Photo</label>
+                            <input type="file" class="form-control-file" name="photo">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+@endsection
---
+++ resources/views/pages/backend/acara/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.backend.admin')
+
+@section('title')
+    Acara
+@endsection
+
+@section('content')
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        </div>
+
+        <!-- Content Row -->
+
+        <div class="row">
+            <div class="card w-100">
+                <h5 class="card-header">
+                    <a href="{{route('acara.create')}}" class="btn btn-primary">Tambah Data</a>
+                </h5>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                          </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+@endsection
